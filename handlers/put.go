9571eb85e29be26955e64f9d5b21a24d3a655af9
Martin E Sesto <sesto@outlook.com>
2021-06-10 10:34:57 -0300

    Added all handlers for the key:value store app, included DELETE

---
+++ handlers/put.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// keyValuePutHandler expects to be called with a PUT request for
+// the "/v1/key/{key}" resource.
+func KeyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Retrieve "key" from the request
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body) // The request body has our value
+	defer r.Body.Close()
+
+	if err != nil { // If we have an error, report it
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value)) // Store the value as a string
+	if err != nil {               // If we have an error, report it
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated) // All good! Return StatusCreated
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	h "github.com/msesto/cloudnative-go/handlers"
 )
 
 func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +16,10 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", helloMuxHandler)
+
+	r.HandleFunc("/v1/{key}", h.KeyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", h.KeyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", h.KeyValueDeleteHandler).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
